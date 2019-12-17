7cee3298c5f47bc32c99e9beb4426cfc20ee7a8f
mruysam <55069064+mruysam@users.noreply.github.com>
2019-12-17 16:49:54 -0300

    Add files via upload

---
+++ 0 a 100 repetição - TheHuxley.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a=0;
+    while (a<100){
+       printf ("%d\n",a);
+      a++;
+}
+	return 0;
+}
